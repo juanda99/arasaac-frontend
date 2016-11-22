46ce7562f6b2a2f3ee497a4beeab144fbce8c28c
juanda <juandacorreo@gmail.com>
2016-11-22 10:46:44 +0100

    refactoring code

--- app/components/Participate/index.js
+++ app/components/Participate/index.js
@@ -4,15 +4,14 @@
 *
 */
 
-import { grey200 } from 'material-ui/styles/colors'
-import { typography } from 'material-ui/styles'
-
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
-import messages from './messages'
 import H3 from 'components/H3'
 import FullWidthSection from 'components/FullWidthSection'
 import RaisedButton from 'material-ui/RaisedButton'
+import { grey200 } from 'material-ui/styles/colors'
+import { typography } from 'material-ui/styles'
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import messages from './messages'
 
 const styles = {
   H3: {
@@ -26,7 +25,6 @@ const Participate = () => (
     <RaisedButton
       label={<FormattedMessage {...messages.contact} />}
       primary={true}
-      linkButton={true}
       href='https://github.com/callemall/material-ui'
       style={styles.button}
     />
