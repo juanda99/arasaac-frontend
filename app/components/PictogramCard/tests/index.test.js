96965b1e0d0d4827e08a0d349d74011db8a80c6a
juanda <juandacorreo@gmail.com>
2016-11-30 10:39:03 +0100

    add Pictogram Card

---
+++ app/components/PictogramCard/tests/index.test.js
@@ -0,0 +1,11 @@
+// import PictogramCard from '../index';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<PictogramCard />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
