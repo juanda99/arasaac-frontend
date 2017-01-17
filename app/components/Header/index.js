532faa9228583a0a96985a4b92f7950947427587
juanda <juandacorreo@gmail.com>
2017-01-17 23:36:58 +0100

    add component

---
+++ app/components/Header/index.js
@@ -0,0 +1,22 @@
+/**
+*
+* Header
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+function Header() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+Header.propTypes = {
+
+};
+
+export default Header;
