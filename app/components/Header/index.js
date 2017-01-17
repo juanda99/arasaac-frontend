52423f39bc55c7e3a7fdb2c284572caca1a8c3b0
juanda <juandacorreo@gmail.com>
2017-01-17 23:45:18 +0100

    Lint

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -4,7 +4,7 @@
 *
 */
 
-import React from 'react';
+import React from 'react'
 // import styled from 'styled-components';
 
 
@@ -12,11 +12,11 @@ function Header() {
   return (
     <div>
     </div>
-  );
+  )
 }
 
 Header.propTypes = {
 
-};
+}
 
-export default Header;
+export default Header
