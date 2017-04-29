0a98f01be2c3a36ba5538e33245f8cc0aa7250b6
juanda <juandacorreo@gmail.com>
2017-04-29 10:01:33 +0200

    implementing new filters

--- app/components/Filters/SelectArea.js
+++ app/components/Filters/SelectArea.js
@@ -21,12 +21,30 @@ class SelectArea extends React.Component {
       <div style={styles.div}>
         {this.state.value !== 1 ? <FilterIcon /> : null}
         <SelectField autoWidth={true} value={this.state.value} onChange={this.handleChange} style={styles.select}>
-          <MenuItem value={1} label={<FormattedMessage {...messages.catalog} />} primaryText={<FormattedMessage {...messages.allCatalogs} />} />
-          <MenuItem value={2} primaryText={<FormattedMessage {...messages.colorPictograms} />} />
-          <MenuItem value={3} primaryText={<FormattedMessage {...messages.blackAndWhitePictograms} />} />
-          <MenuItem value={4} primaryText={<FormattedMessage {...messages.pictures} />} />
-          <MenuItem value={5} primaryText={<FormattedMessage {...messages.lseVideos} />} />
-          <MenuItem value={5} primaryText={<FormattedMessage {...messages.lseColor} />} />
+          <MenuItem value={1} primaryText={<FormattedMessage {...messages.selfawareness} />} />
+          <MenuItem value={2} primaryText={<FormattedMessage {...messages.visualdiscrimination} />} />
+          <MenuItem value={3} primaryText={<FormattedMessage {...messages.auditorydiscrimination} />} />
+          <MenuItem value={4} primaryText={<FormattedMessage {...messages.phonology} />} />
+          <MenuItem value={5} primaryText={<FormattedMessage {...messages.morphosyntax} />} />
+          <MenuItem value={6} primaryText={<FormattedMessage {...messages.semantics} />} />
+          <MenuItem value={7} primaryText={<FormattedMessage {...messages.pragmatics} />} />
+          <MenuItem value={8} primaryText={<FormattedMessage {...messages.reading} />} />
+          <MenuItem value={9} primaryText={<FormattedMessage {...messages.writing} />} />
+          <MenuItem value={10} primaryText={<FormattedMessage {...messages.literature} />} />
+          <MenuItem value={11} primaryText={<FormattedMessage {...messages.languages} />} />
+          <MenuItem value={12} primaryText={<FormattedMessage {...messages.numeration} />} />
+          <MenuItem value={13} primaryText={<FormattedMessage {...messages.basicOperations} />} />
+          <MenuItem value={14} primaryText={<FormattedMessage {...messages.problems} />} />
+          <MenuItem value={15} primaryText={<FormattedMessage {...messages.geometry} />} />
+          <MenuItem value={16} primaryText={<FormattedMessage {...messages.naturalSciences} />} />
+          <MenuItem value={17} primaryText={<FormattedMessage {...messages.socialSciences} />} />
+          <MenuItem value={18} primaryText={<FormattedMessage {...messages.music} />} />
+          <MenuItem value={19} primaryText={<FormattedMessage {...messages.art} />} />
+          <MenuItem value={20} primaryText={<FormattedMessage {...messages.physicalEducation} />} />
+          <MenuItem value={21} primaryText={<FormattedMessage {...messages.religion} />} />
+          <MenuItem value={22} primaryText={<FormattedMessage {...messages.health} />} />
+          <MenuItem value={23} primaryText={<FormattedMessage {...messages.leisure} />} />
+          <MenuItem value={24} primaryText={<FormattedMessage {...messages.signage} />} />
         </SelectField>
       </div>
     )
--- app/components/Filters/messages.js
+++ app/components/Filters/messages.js
@@ -110,5 +110,225 @@ export default defineMessages({
     id: 'app.components.Filters.SelectSize.smallChoose',
     description: 'Menu choose small size',
     defaultMessage: 'Small, wide less than 500px'
+  },
+  selfawareness: {
+    id: 'app.components.Filters.SelectArea.selfawareness',
+    description: 'Menu choose material area',
+    defaultMessage: 'Self-awareness and personal autonomy'
+  },
+  visualdiscrimination: {
+    id: 'app.components.Filters.SelectArea.visualdiscrimination',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Visual discrimination'
+  },
+  auditorydiscrimination: {
+    id: 'app.components.Filters.SelectArea.auditorydiscrimination',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Auditory discrimination'
+  },
+  phonology: {
+    id: 'app.components.Filters.SelectArea.phonology',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Phonology'
+  },
+  morphosyntax: {
+    id: 'app.components.Filters.SelectArea.morphosyntax',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Morphosyntax'
+  },
+  semantics: {
+    id: 'app.components.Filters.SelectArea.semantics',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Semantics'
+  },
+  pragmatics: {
+    id: 'app.components.Filters.SelectArea.pragmatics',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Pragmatics'
+  },
+  reading: {
+    id: 'app.components.Filters.SelectArea.reading',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Reading'
+  },
+  writing: {
+    id: 'app.components.Filters.SelectArea.writing',
+    description: 'Menu choose material area',
+    defaultMessage: 'Language: Writing'
+  },
+  literature: {
+    id: 'app.components.Filters.SelectArea.literature',
+    description: 'Menu choose material area',
+    defaultMessage: 'Literature'
+  },
+  languages: {
+    id: 'app.components.Filters.SelectArea.languages',
+    description: 'Menu choose material area',
+    defaultMessage: 'Languages'
+  },
+  numeration: {
+    id: 'app.components.Filters.SelectArea.numeration',
+    description: 'Menu choose material area',
+    defaultMessage: 'Math: numeration'
+  },
+  basicOperations: {
+    id: 'app.components.Filters.SelectArea.basicOperations',
+    description: 'Menu choose material area',
+    defaultMessage: 'Math: Basic operations'
+  },
+  problems: {
+    id: 'app.components.Filters.SelectArea.problems',
+    description: 'Menu choose material area',
+    defaultMessage: 'Math: Problems'
+  },
+  geometry: {
+    id: 'app.components.Filters.SelectArea.geometry',
+    description: 'Menu choose material area',
+    defaultMessage: 'Math: Geometry'
+  },
+  naturalSciences: {
+    id: 'app.components.Filters.SelectArea.naturalSciences',
+    description: 'Menu choose material area',
+    defaultMessage: 'Natural Sciences'
+  },
+  socialSciences: {
+    id: 'app.components.Filters.SelectArea.socialSciences',
+    description: 'Menu choose material area',
+    defaultMessage: 'Social Sciences'
+  },
+  music: {
+    id: 'app.components.Filters.SelectArea.music',
+    description: 'Menu choose material area',
+    defaultMessage: 'Music'
+  },
+  art: {
+    id: 'app.components.Filters.SelectArea.art',
+    description: 'Menu choose material area',
+    defaultMessage: 'Art'
+  },
+  physicalEducation: {
+    id: 'app.components.Filters.SelectArea.physicalEducation',
+    description: 'Menu choose material area',
+    defaultMessage: 'Physical Education'
+  },
+  religion: {
+    id: 'app.components.Filters.SelectArea.religion',
+    description: 'Menu choose material area',
+    defaultMessage: 'Religion'
+  },
+  health: {
+    id: 'app.components.Filters.SelectArea.health',
+    description: 'Menu choose material area',
+    defaultMessage: 'Health'
+  },
+  leisure: {
+    id: 'app.components.Filters.SelectArea.leisure',
+    description: 'Menu choose material area',
+    defaultMessage: 'Leisure and culture'
+  },
+  multimediaBook: {
+    id: 'app.components.Filters.SelectArea.multimediaBook',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Multimedia Book'
+  },
+  picaa: {
+    id: 'app.components.Filters.SelectArea.picaa',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Picaa activity'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
+  },
+  signage: {
+    id: 'app.components.Filters.SelectArea.signage',
+    description: 'Menu choose material activity',
+    defaultMessage: 'Signage'
   }
 })
