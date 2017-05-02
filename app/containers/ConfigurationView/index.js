f6ffec5ff11a1be499808de7903f5bb73ebd89a3
juanda <juandacorreo@gmail.com>
2017-05-02 14:09:54 +0200

    implementing configuration filters for materials

--- app/containers/ConfigurationView/index.js
+++ app/containers/ConfigurationView/index.js
@@ -9,7 +9,7 @@ import { FormattedMessage } from 'react-intl'
 import { Row, Col } from 'react-flexbox-grid'
 // import Divider from 'material-ui/Divider'
 import Paper from 'material-ui/Paper'
-import H1 from 'components/H1'
+import H2 from 'components/H2'
 // import H2 from 'components/H2'
 import H3 from 'components/H3'
 import ToggleFilter from 'containers/ToggleFilter'
@@ -25,15 +25,25 @@ const style = {
 
 const ConfigurationView = () => (
   <View>
-    <H1>{<FormattedMessage {...messages.searchPictograms} />}</H1>
     <Row>
-      <Col xs={12} md={6}>
+      <Col xs={12} lg={6}>
+        <H2>{<FormattedMessage {...messages.searchPictograms} />}</H2>
         <Paper style={style} zDepth={2} rounded={false}>
           <H3>{<FormattedMessage {...messages.filters} />}</H3>
-          <p>{<FormattedMessage {...messages.filtersDesc} />}</p>
+          <p>{<FormattedMessage {...messages.pictogramFiltersDesc} />}</p>
           <ToggleFilter label={<FormattedMessage {...messages.filterCatalog} />} type='pictograms' filter='catalog' />
           <ToggleFilter label={<FormattedMessage {...messages.filterLicense} />} type='pictograms' filter='license' />
           <ToggleFilter label={<FormattedMessage {...messages.filterSize} />} type='pictograms' filter='size' />
+        </Paper>
+      </Col>
+      <Col xs={12} lg={6}>
+        <H2>{<FormattedMessage {...messages.searchMaterials} />}</H2>
+        <Paper style={style} zDepth={2} rounded={false}>
+          <H3>{<FormattedMessage {...messages.filters} />}</H3>
+          <p>{<FormattedMessage {...messages.materialFiltersDesc} />}</p>
+          <ToggleFilter label={<FormattedMessage {...messages.filterCatalog} />} type='materials' filter='catalog' />
+          <ToggleFilter label={<FormattedMessage {...messages.filterLicense} />} type='materials' filter='license' />
+          <ToggleFilter label={<FormattedMessage {...messages.filterSize} />} type='materials' filter='size' />
         </Paper>
       </Col>
     </Row>
