6ab2d936dce48ab86e0d9d8959f7db2997b27cfe
juanda <juandacorreo@gmail.com>
2017-05-11 09:48:06 +0200

    remove extra code

--- app/utils/createComponentWithIntl.js
+++ app/utils/createComponentWithIntl.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { IntlProvider } from 'react-intl'
-import { render } from 'enzyme'
 
 const createComponentWithIntl = (children, props = { locale: 'en' }) =>
   renderer.create(
-    <IntlProvider {...props}>
-      {children}
-    </IntlProvider>
-  )
-
-
-export const intlEnzymeComponent = (children, props = { locale: 'en' }) =>
-  render(
     <IntlProvider {...props}>
       {children}
     </IntlProvider>
