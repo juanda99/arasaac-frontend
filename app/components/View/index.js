8a53c7aad0358d5508fcee0f37f656a9cb4c4ad8
juanda <juandacorreo@gmail.com>
2017-06-13 22:27:10 +0200

    fix views for mobile

--- app/components/View/index.js
+++ app/components/View/index.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
+import media from 'utils/mediaqueries'
 
 const View = styled.div`
   padding: 8rem 4rem;
+  ${media.xs} {
+    padding: 6rem 1rem;
+  }
+
 `
 
 export default View
