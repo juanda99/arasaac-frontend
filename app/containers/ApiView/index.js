6a8f2d6b78793f46cc451c59cb049cd63a43fe49
juanda <juandacorreo@gmail.com>
2017-06-27 14:23:17 +0200

    upgrade swagger-ui, api doc works

--- app/containers/ApiView/index.js
+++ app/containers/ApiView/index.js
@@ -7,48 +7,25 @@
 import React, { Component } from 'react'
 import SwaggerUi, { presets } from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css'
+import View from 'components/View'
 
 class ApiView extends Component {
   componentDidMount() {
     SwaggerUi({
       dom_id: '#swaggerContainer',
-      url: 'http://api.arasaac.org/arasaac.json',
+      url: 'https://api.arasaac.org/arasaac.json',
       presets: [presets.apis]
     })
   }
 
   render() {
     return (
-      <div id='swaggerContainer' />
+      <View>
+        <div id='swaggerContainer'>
+        </div>
+      </View>
     )
   }
 }
 
 export default ApiView
-
-
-/*
-import React from 'react'
-import View from 'components/View'
-import Iframe from 'react-iframe'
-
-const container = {
-  position: 'relative',
-  // height: '1000',
-  overflow: 'auto',
-  paddingBottom: '75%',
-  zIndex: '10000'
-}
-
-const ApiView = () => (
-  <View style={{ height: '10000px' }}>
-    <div style={{ container }}>
-      <Iframe url='//api.arasaac.org/api-docs' width={'100%'} height={'100%'} />
-    </div>
-  </View>
-)
-
-
-export default ApiView
-
-*/
