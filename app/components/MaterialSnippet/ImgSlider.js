d75577e885e6fa3ba439254ae78698911389caab
juanda <juandacorreo@gmail.com>
2017-12-04 14:58:32 +0100

    refactor materialsnippet

---
+++ app/components/MaterialSnippet/ImgSlider.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactSlidy from 'react-slidy'
+import '!!style-loader!css-loader!./index.css'
+
+const styles = {
+  snippetImg: {
+    flexGrow: 1,
+    width: '300px'
+  }
+}
+
+
+class ImgSlider extends Component {
+  render() {
+    const { id, images } = this.props
+    return (
+      <div style={styles.snippetImg}>
+        <div style={{ display: 'block' }}>
+          <ReactSlidy dynamicContent infinite={false}>
+            {
+              images.length ?
+                images.map((image, key) => (
+                  <img key={key} src={`//static.arasaac.org/${id}/screenshots/${image}`} alt='screenshot' />
+                ))
+              : <img src={'//static.arasaac.org/noimage.png'} alt='Not available' />
+            }
+          </ReactSlidy>
+        </div>
+      </div>
+    )
+  }
+}
+
+ImgSlider.propTypes = {
+  images: PropTypes.array,
+  id: PropTypes.number.isRequired
+}
+
+export default ImgSlider
--- app/components/MaterialSnippet/index.js
+++ app/components/MaterialSnippet/index.js
@@ -6,24 +6,17 @@ import Avatar from 'material-ui/Avatar'
 import { lightGreen400, lightGreen800 } from 'material-ui/styles/colors'
 import ActivityIcon from 'material-ui/svg-icons/action/input'
 import AreaIcon from 'material-ui/svg-icons/social/school'
-import ReactSlidy from 'react-slidy'
-import '!!style-loader!css-loader!./index.css'
 import ReadMore from 'components/ReadMore'
 import H2 from 'components/H2'
 import Ribbon from 'components/Ribbon'
 import activity from 'data/activity'
 import area from 'data/area'
+import SnippetImg from './ImgSlider'
 import Item from './Item'
 
 const styles = {
   chip: {
     margin: '4px'
-  },
-  labelChip: {
-    fontSize: '0.7rem',
-    paddingLeft: '8px',
-    paddingRight: '8px',
-    lineHeight: '1.8rem'
   },
   wrapper: {
     display: 'flex',
@@ -33,10 +26,6 @@ const styles = {
     flexGrow: 3,
     width: '600px',
     padding: '1rem'
-  },
-  snippetImg: {
-    flexGrow: 1,
-    width: '300px'
   },
   snippet: {
     display: 'flex',
@@ -106,7 +95,6 @@ export class MaterialSnippet extends PureComponent {
 
   render() {
     const { material, locale, filtersMap, showLabels } = this.props
-
     const activityTags = material.activity.map((id) => {
       if (filtersMap.get('activity').includes(id)) {
         return (
@@ -114,7 +102,6 @@ export class MaterialSnippet extends PureComponent {
             backgroundColor={lightGreen400}
             style={styles.chip}
             key={id}
-            labelStyle={styles.labelChip}
             onClick={(e) => this.handleTouchTap('activity', id, 0, e)}
           >
             <Avatar color={'white'} size={30} backgroundColor={lightGreen800} icon={<ActivityIcon />} />
@@ -145,7 +132,7 @@ export class MaterialSnippet extends PureComponent {
             onClick={(e) => this.handleTouchTap('area', id, 0, e)}
             labelStyle={styles.labelChip}
           >
-            <Avatar color={'white'} size={15} backgroundColor={lightGreen800} icon={<AreaIcon iconStyle={{width: '10px', height: '10px'}} />} />
+            <Avatar color={'white'} size={15} backgroundColor={lightGreen800} icon={<AreaIcon iconStyle={{ width: '10px', height: '10px' }} />} />
             {area[id]}
           </Chip>
         )
@@ -163,6 +150,8 @@ export class MaterialSnippet extends PureComponent {
         </Chip>
       )
     })
+
+    const images = [...material.commonScreenshots || [], ...material.screenshots[locale] || []]
     // const languageTags = material.translations.map((translation) => <Chip style={styles.chip} key={translation.language}><Avatar color='#444' icon={<LanguageIcon />} />{language[translation.language]}</Chip>)
     // languageTags.push(<Chip style={styles.chip} key={material.language}><Avatar color='#222' icon={<LanguageIcon />} />{language[material.language]}</Chip>)
     return (
@@ -170,19 +159,7 @@ export class MaterialSnippet extends PureComponent {
         <Ribbon />
 
         <div style={styles.snippet}>
-          <div style={styles.snippetImg}>
-            <div style={{display: 'block'}}>
-              <ReactSlidy dynamicContent infinite={false}>
-                {
-                  material.images.length ?
-                    material.images.map((image, key) => (
-                      <img key={key} src={`//static.arasaac.org/${material.idMaterial}/screenshots/${image}`} alt={`${material.title}`} />
-                    ))
-                  : <img src={'//static.arasaac.org/noimage.png'} alt='Not available' />
-                }
-              </ReactSlidy>
-            </div>
-          </div>
+          <SnippetImg images={images} id={material.idMaterial} />
           <div style={styles.snippetText}>
             <H2 primary ucase>{material.title}</H2>
             <ReadMore>
@@ -195,7 +172,6 @@ export class MaterialSnippet extends PureComponent {
           </div>
         </div>
       </Item>
-
     )
   }
 }
