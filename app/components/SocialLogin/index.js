ad320bfeba6f9844a47fe75a11ebde562f7c2ea2
juanda <juandacorreo@gmail.com>
2018-03-07 13:49:20 +0100

    fix bug facebook login on mobile

--- app/components/SocialLogin/index.js
+++ app/components/SocialLogin/index.js
@@ -1,8 +1,22 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import Div from 'components/Div'
+import RaisedButton from 'material-ui/RaisedButton'
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import { FormattedMessage } from 'react-intl'
+import { blue500, white } from 'material-ui/styles/colors'
 import GoogleLogin from './GoogleLogin'
-import FacebookLogin from './FacebookLogin'
+import messages from './messages'
+import FacebookIcon from './icons/FacebookIcon'
+
+const styles = {
+  facebookButton: {
+    float: 'right',
+    width: '100%',
+    marginBottom: 10,
+    top: -10
+  }
+}
 
 class SocialLogin extends PureComponent {
   /* eslint-disable no-console */
@@ -24,17 +38,30 @@ class SocialLogin extends PureComponent {
   render() {
     return (
       <Div top={2} >
+        <FacebookLogin
+          appId='1687810071473822'
+          autoLoad={true}
+          fields='name,email,picture'
+          callback={this.responseFacebook}
+          render={(renderProps) => (
+            <RaisedButton
+              onClick={renderProps.onClick}
+              style={styles.facebookButton}
+              backgroundColor={blue500}
+              label={<FormattedMessage {...messages.facebook} />}
+              icon={<FacebookIcon />}
+              labelColor={white}
+            />
+          )}
+        >
+        </FacebookLogin>
         <GoogleLogin
           clientId={'856321241205-djlltqe6cpo9vm3hp392giboofdp44ha.apps.googleusercontent.com'}
           onSuccess={this.success}
           onFailure={this.error}
           offline={false}
         />
-        <FacebookLogin
-          appId='1687810071473822'
-          fields='name,email'
-          callback={this.responseFacebook}
-        />
+
       </Div>
     )
   }
