42d6ca651e865c0d67b0a7b431562441816be92b
juanda <juandacorreo@gmail.com>
2018-04-03 23:42:08 +0200

    change arasaac.json route

--- app/containers/ApiView/index.js
+++ app/containers/ApiView/index.js
@@ -15,10 +15,9 @@ import './theme-material.css'
 
 class ApiView extends PureComponent {
   componentDidMount() {
-    console.log(`JJJJJJJJJJJJJJJJJJ: ${API_SERVER}/public/arasaac.json`)
     SwaggerUi({
       dom_id: '#swaggerContainer',
-      url: `${API_SERVER}/public/arasaac.json`,
+      url: `${API_SERVER}/arasaac.json`,
       presets: [presets.apis]
     })
   }
