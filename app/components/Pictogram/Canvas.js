02d207f7f884d8bec49fe847325cec4d899899d5
juanda <juandacorreo@gmail.com>
2019-01-24 12:54:34 +0100

    fix error background invisible

--- app/components/Pictogram/Canvas.js
+++ app/components/Pictogram/Canvas.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 const Canvas = styled.div`
-background-color: #FEFEFE,
-background-image: linear-gradient(45deg, #CBCBCB 25%, transparent 25%, transparent 75%, #CBCBCB 75%, #CBCBCB), linear-gradient(45deg, #CBCBCB 25%, transparent 25%, transparent 75%, #CBCBCB 75%, #CBCBCB),
-background-size: 10px 10px,
-background-position: 0 0, 5px 5px,
-background-repeat: repeat
+background-color: #FEFEFE;
+background-image: linear-gradient(45deg, #CBCBCB 25%, transparent 25%, transparent 75%, #CBCBCB 75%, #CBCBCB), linear-gradient(45deg, #CBCBCB 25%, transparent 25%, transparent 75%, #CBCBCB 75%, #CBCBCB);
+background-size: 10px 10px;
+background-position: 0 0, 5px 5px;
+background-repeat: repeat;
 `
 
 export default Canvas
