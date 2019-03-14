bbe0f2d2e2b5df314cea6da2be96692c3f665c13
juanda <juandacorreo@gmail.com>
2019-03-14 14:15:24 +0100

    add licences, fix H3 format

--- app/components/License/EnLicense.js
+++ app/components/License/EnLicense.js
@@ -1,64 +1,76 @@
 import React from 'react'
 import P from 'components/P'
+import H3 from 'components/H3'
+import Divider from 'material-ui/Divider'
+import CreativeCommons from './CreativeCommons'
+import Arasaac from './Arasaac'
 const EnLicense = () => (
   <div>
     <P>
-      Diputación General de Aragón (Aragon Government) has the property of the
-      original files from which the pictograms published in this Web are
-      obtained.
+      <Arasaac /> is a brand of the Government of Aragon (Spain), registered in
+      the Spanish Patent and Trademark Office. The <Arasaac /> pictogram
+      collection is registered in the General Registry of Intellectual Property
+      (No. Deposit Legal Z 901-2013) as a collective work on behalf of the
+      Diputación General de Aragón, who has edited and disseminated it in
+      accordance with the provisions of Article 8 of the revised text of the
+      Intellectual Property Law, approved by Royal Legislative Decree 1/1996, of
+      April 12.
     </P>
     <P>
-      Resources offered in the site (pictograms, images or videos), as well as
-      the materials based on them, are released under a Creative Commons License
-      (BY-NC-SA), being authorised for NON COMMERCIAL uses if author and source
-      are mentioned and the use of materials is shared in the same way.
+      Resources offered on the website (pictograms, images, locutions or
+      videos), as well as the materials based on them, are published under{' '}
+      <CreativeCommons locale='en' />, authorizing their use for non-profit
+      purposes provided that the source, author and are shared under the same
+      license.
     </P>
     <P>
-      That implies that any work based on resources hosted ARASAAC's catalogues
-      (pictograms, images and videos) must be distributed with the same Creative
-      Commons License, author (Sergio Palao) must be quoted and origin (ARASAAC
-      http://arasaac.org/) must be recognised, as well as the Creative Commons'
-      License.
+      Therefore, the use of these resources within any product or publication
+      for commercial purposes is excluded.
     </P>
     <P>
-      Means of expressing this can be: <br />
-      The pictograms used are property of Aragon Government and have been
-      created by Sergio Palao to ARASAAC (http://arasaac.org) which distribute
-      them under Creative Commons License (BY-NC-SA)
+      For any other use they are strictly prohibited, without written
+      authorization of the copyright holders, under the sanctions established by
+      law, the total or partial reproduction of these resources by any means or
+      procedure, including reprography and computer processing.
+    </P>
+
+    <P>
+      Any work derived from the resources contained in the <Arasaac /> catalogs
+      (pictograms, images, voiceovers or videos), must distribute with the same
+      <CreativeCommons locale='en' />, it must be cited the author and the owner
+      thereof (Gobierno de Aragón), their origin (
+      <Arasaac />) and the license under which is distributed (
+      <CreativeCommons locale='en' />
+      ).
+    </P>
+
+    <P>
+      In the case of pictograms, the way to attribute authorship is: <br />
+      <em>
+        The pictographic symbols used are the property of the Government of
+        Aragón and have been created by Sergio Palao for <Arasaac link={true} />
+        that distributes them under <CreativeCommons locale='en' />.
+      </em>
     </P>
 
     <P>
       Or in a schematic way: <br />
-      Pictograms author: Sergio Palao <br />
-      Origin: ARASAAC (http://arasaac.org)
-      <br />
-      License: CC (BY-NC-SA)
-      <br />
-      Property: Aragon Goverment
+      <em>
+        Pictograms author: Sergio Palao <br />
+        Origin: <Arasaac link={true} />
+        <br />
+        License: CC (BY-NC-SA)
+        <br />
+        Owner: Government of Aragon (Spain)
+      </em>
     </P>
+    <Divider />
+    <H3>Disclaimer</H3>
     <P>
-      Remains excluded from this permission the use of these materials inside
-      any product, sotware, publications with commercial purposes.
-    </P>
-    <P>
-      For any other use reprography and the computer treatment remain rigorously
-      prohibited, without the written authorization of the holders of the
-      "Copyright", under the sanctions established by the laws, the total or
-      partial reproduction of these materials for any way or procedure.
-    </P>
-    <P>Responsibility’s exemption clause:</P>
-    <P>
-      The Aragonese Portal of Augmentative and Alternative Communication has an
-      informative and illustrative character, and tries to put at the disposal
-      of any interested person the information of the project. Nevertheless, it
-      is necessary to specify the following thing:
-    </P>
-    <P>
-      The information that offers is merely informative and lacks of any effect
-      binding the Administration, so that we do not assume any responsibility of
-      the content. The information that offers might not be exhaustive, exact or
-      updated. Links are facilitated to external pages on which ARASAAC does not
-      have any control, and respect of which we decline any responsibility.
+      The Government of Aragon, is not responsible for the improper or negligent
+      use of resources contained in the <Arasaac link={true} /> catalogs made by
+      third parties that exceeds the terms of the{' '}
+      <CreativeCommons locale='en' />.
     </P>
   </div>
 )
