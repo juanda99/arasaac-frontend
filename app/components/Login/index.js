1728f7b38ce9f90bf64372da68c8e858246bb131
juanda <juandacorreo@gmail.com>
2019-05-03 10:41:04 +0200

    fix linter issues

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -1,4 +1,4 @@
-export LoginForm from './LoginForm'
-export RegisterForm from './RegisterForm'
-export RegisterOptions from './RegisterOptions'
-export RecoverPasswordForm from './RecoverPasswordForm'
+export { default as LoginForm } from './LoginForm'
+export { default as RegisterForm } from './RegisterForm'
+export { default as RegisterOptions } from './RegisterOptions'
+export { default as RecoverPasswordForm } from './RecoverPasswordForm'
