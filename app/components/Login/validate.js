870efceea7831e01bd89f295958dd3647cdc550d
juanda <juandacorreo@gmail.com>
2019-10-24 09:20:00 +0200

    add url validation for signup form

--- app/components/Login/validate.js
+++ app/components/Login/validate.js
@@ -1,6 +1,10 @@
 // validation functions
-export const required = (value) => (value == null ? 'Required' : undefined)
 export const email = (value) => {
   if (!value) return false
   return value && /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+}
+
+export const url = (value) => {
+  const regexp = /^(http: \/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gim
+  return regexp.test(value)
 }
