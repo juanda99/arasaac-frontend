6e672684812ce2c78ac19187379484978622a010
juanda <juandacorreo@gmail.com>
2019-10-25 14:26:54 +0200

    fix bug if url empty

--- app/components/Login/validate.js
+++ app/components/Login/validate.js
@@ -5,6 +5,7 @@ export const email = (value) => {
 }
 
 export const url = (value) => {
+  if (!value) return true
   const regexp = /^(http: \/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gim
   return regexp.test(value)
 }
