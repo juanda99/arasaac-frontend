c3f1e44c38b76c3a3b1278165aa81e6a08a55e5a
juanda <juandacorreo@gmail.com>
2019-12-02 23:15:41 +0100

    add languages as array

--- app/utils/index.js
+++ app/utils/index.js
@@ -70,3 +70,86 @@ export const keywordSelector = (searchText, keywords) => {
 /* for hair and skin options, get object key for a given value */
 export const getKeyByValue = (object, value) =>
   Object.keys(object).find((key) => object[key] === value)
+
+export const languages = [
+  {
+    code: 'es',
+    text: 'Español'
+  },
+  {
+    code: 'en',
+    text: 'English'
+  },
+  {
+    code: 'ar',
+    text: 'عربى'
+  },
+  {
+    code: 'bg',
+    text: 'български'
+  },
+  {
+    code: 'br',
+    text: 'Português do Brasil'
+  },
+  {
+    code: 'ca',
+    text: 'Català'
+  },
+  {
+    code: 'de',
+    text: 'Deutsche'
+  },
+  {
+    code: 'eu',
+    text: 'Euskal'
+  },
+  {
+    code: 'fr',
+    text: 'Français'
+  },
+  {
+    code: 'gl',
+    text: 'Galego'
+  },
+  {
+    code: 'he',
+    text: 'עברי' // hebreo
+  },
+  {
+    code: 'hr',
+    text: 'Hrvatski'
+  },
+  {
+    code: 'it',
+    text: 'Italiano'
+  },
+  {
+    code: 'nl',
+    text: 'Nederlands'
+  },
+  {
+    code: 'pl',
+    text: 'Polskie'
+  },
+  {
+    code: 'pt',
+    text: 'Português'
+  },
+  {
+    code: 'ro',
+    text: 'Română'
+  },
+  {
+    code: 'ru',
+    text: 'Pусский'
+  },
+  {
+    code: 'val',
+    text: 'Valencia'
+  },
+  {
+    code: 'zh',
+    text: '简体中文）'
+  }
+]
