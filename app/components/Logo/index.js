3d31a015e83d4cba25fab938911c43da3b51d255
juanda <juandacorreo@gmail.com>
2020-01-14 15:49:38 +0100

    add new logo

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -2,17 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Div from 'components/Div'
 import ArasaacLogo from './arasaac-logo.svg'
+import { CardMedia, Card } from 'material-ui/Card'
+
 
 const style = {
-  width: 250,
+  width: 270,
   display: 'flex',
-  margin: '0 auto'
+  margin: '0 auto',
+  padding: 20,
+  borderRadius: '50%',
+  backgroundColor: 'white'
 }
 
 const Logo = ({ src, top }) => (
   <Div top={top}>
     <img alt='Arasaac logo' style={style} src={src || ArasaacLogo} />
-  </Div>
+  </Div >
 )
 
 Logo.propTypes = {
