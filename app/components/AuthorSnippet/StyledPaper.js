3431619251c3542c52c66a5faa3529ebcd81a828
juanda <juandacorreo@gmail.com>
2020-01-27 12:24:20 +0100

    fix logo react card flip color and position

--- app/components/AuthorSnippet/StyledPaper.js
+++ app/components/AuthorSnippet/StyledPaper.js
@@ -7,6 +7,11 @@ const StyledPaper = styled(Paper)`
   margin-bottom: 30px;
   width: 300px;
   height: 400px;
+  ${media.xs} {
+    box-sizing: border-box;
+    width: 95%;
+    margin: 20px auto;
+  }
 `
 
 export default muiThemeable()(StyledPaper)
