ae6e95453c6471f05ed1445049ddda8822956791
juanda <juandacorreo@gmail.com>
2020-04-24 01:08:09 +0200

    fix margin size, and fix rtl no margin

--- app/components/Header/Title.js
+++ app/components/Header/Title.js
@@ -7,7 +7,9 @@
 import styled from 'styled-components'
 
 const Title = styled.span`
-  padding-left: ${(props) => (props.docked ? '15em' : '0em')};
+  padding-left: ${(props) => (props.docked ? '12em' : '0em')};
+  ${'' /* for rtl: */}
+  padding-right: ${(props) => (props.docked ? '12em' : '0em')};
 `
 
 export default Title
