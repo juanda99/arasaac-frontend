239167ba1f4cecb128db72c40fde43cc564d25da
juanda <juandacorreo@gmail.com>
2020-10-28 10:59:10 +0100

    fix lse layout

--- app/containers/LSE/LSEItem.js
+++ app/containers/LSE/LSEItem.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import P from 'components/P'
+import ReactPlayer from 'react-player'
 import ShowSoundPlayer from 'components/SoundPlayer/ShowSoundPlayer'
 import { STATIC_SERVER } from 'services/config'
 import H2 from 'components/H2'
 import H3 from 'components/H3'
+
+
+const styles = {
+
+  snippetText: {
+    flexGrow: 3,
+    width: '600px',
+    padding: '1rem'
+  },
+  snippet: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%'
+  },
+  snippetImgs: {
+    width: '100%',
+    maxWidth: '500px',
+    height: 'auto'
+  },
+  actionBtn: {
+    margin: '5px'
+  },  
+  video: {
+    width: '100%',
+    maxWidth: '500px',
+    height: '500px'
+  }
+}
 
 class LSEItem extends Component {
 
@@ -14,44 +42,54 @@ class LSEItem extends Component {
     const videosAcepciones = data.items
       .filter(item => item.tipo===11)
       .map(item => (
-          <video key={`${item.idImagen}-video`} width="320" height="240" controls poster={true}>
-            <source 
-            src={`${STATIC_SERVER}/lse-acepciones/${item.idImagen}.mp4`} 
-            poster={`${STATIC_SERVER}/lse-images/${data.idPalabra}.jpg`} 
-            type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          <ReactPlayer key={`${item.idImagen}-video`}
+            url={`${STATIC_SERVER}/lse-acepciones/${item.idImagen}.mp4`}  
+            style={styles.video}
+            width='500px' height='500px'
+            controls
+          />
       ))
     
     const imagesAcepciones = data.items
       .filter(item => item.tipo===12) 
       .map(item => (
-          <img key={`${item.idImagen}-image`} src={`${STATIC_SERVER}/lse-images/${item.idImagen}.jpg`} />
+          <img  
+            key={`${item.idImagen}-image`} 
+            style={styles.video}
+            src={`${STATIC_SERVER}/lse-images/${item.idImagen}.jpg`} 
+          />
       ))
     
 
     return (
       <div style={{padding: 30}}>
-        <ShowSoundPlayer hasLocution={true} locale='es' keyword={searchText} download={false} />
-        <ShowSoundPlayer hasLocution={true} locale='es' keyword={searchText} download={true}  />
-        <H2 primary><strong>{searchText}: </strong>{data.desc} </H2>
-        <div style={{display:'flex'}}>
-          <div  style={{margin: 20, textAlign: 'center'}} >
+        <div style={{display: 'flex'}}>
+          <ShowSoundPlayer hasLocution={true} locale='es' keyword={searchText} download={false} />
+          <ShowSoundPlayer hasLocution={true} locale='es' keyword={searchText} download={true}  />
+          <H2 primary ucase noMargin primary><strong>{searchText}</strong></H2>
+
+        </div>
+        <H3 noMargin={true} primary>{data.desc} </H3>
+        <div style={{display: 'flex', flexWrap: 'wrap',  justifyContent: 'space-between'}}>
             {imagesAcepciones}
-          </div>
-          <div  style={{margin: 20, textAlign: 'center'}} >
+        </div>
+          <div  style={{margin: 20}} >
             <H3 primary ucase>Acepción LSE</H3>
+            <div style={{display: 'flex', flexWrap: 'wrap',  justifyContent: 'space-between'}}>
             {videosAcepciones}
+            </div>
+
           </div>
-          <div  style={{margin: 20, textAlign: 'center'}} >
+          <div  style={{margin: 20}} >
             <H3 primary ucase>Definición LSE</H3>
-            <video width="320" height="240" controls  poster={true}>
-              <source src={`${STATIC_SERVER}/lse-definiciones/${data.idPalabra}.mp4`} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+            <ReactPlayer key={`${searchText}-video-def`}
+              url={`${STATIC_SERVER}/lse-definiciones/${data.idPalabra}.mp4`} 
+              style={styles.video}
+              width='500px' height='500px'
+              controls
+            />
           </div>
         </div>
-      </div>
     )
   }
 }
