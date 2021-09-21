88804ffe42b24bc989b29a3454dd29613f18234b
juanda <juandacorreo@gmail.com>
2021-09-21 11:17:36 +0200

    fix translation string

--- app/containers/UploadTranslationView/messages.js
+++ app/containers/UploadTranslationView/messages.js
@@ -7,7 +7,7 @@ export default defineMessages({
       'Use this form exclusively to upload your translation of the material into another language. You should translate all files.',
   },
   contactus: {
-    id: 'app.components.uploadTranslatorView.intro',
+    id: 'app.components.uploadTranslatorView.contactus',
     defaultMessage: 'If in doubt, contact us.',
   },
 })
