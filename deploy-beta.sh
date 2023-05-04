a20f0759a0ca93a3e29723dcf8cf3faacea5274f
juanda <juandacorreo@gmail.com>
2023-05-04 13:55:49 +0200

    add beta deploy

---
+++ deploy-beta.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get the version from the parameter
+VERSION=$1
+
+# If version is empty, exit with warning
+if [ -z "$VERSION" ]
+then
+  echo "No version specified. Usage: ./deploy-beta.sh <version>"
+  exit 1
+fi
+
+if [[ $VERSION != *"beta"* ]]; then
+  echo "Version must include 'beta'."
+  exit 1
+fi
+
+# Build the Docker image
+docker build -f Dockerfile.beta -t cateduac/arasaac-frontend:$VERSION .
+
+# Push the Docker image to Docker Hub
+docker push cateduac/arasaac-frontend:$VERSION
